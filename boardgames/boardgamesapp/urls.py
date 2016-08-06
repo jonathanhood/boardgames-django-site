8066e5b8210a010b303e41080d9afa28d3acd1ef
Jonathan Hood <me@jonathanhood.org>
2016-08-06 14:05:11 -0500

    Initial commit of site

---
+++ boardgames/boardgamesapp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^accounts/login/$', auth_views.login),
+    url(r'^$', views.index, name='index')
+]
+
