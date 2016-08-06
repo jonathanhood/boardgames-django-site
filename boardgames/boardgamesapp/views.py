49b054e3b654d9957f8d5b026baf813ee28c9811
Jonathan Hood <me@jonathanhood.org>
2016-08-06 16:25:15 -0500

    Add file upload capability

--- boardgames/boardgamesapp/views.py
+++ boardgames/boardgamesapp/views.py
@@ -4,11 +4,15 @@ from django.contrib.auth.decorators import login_required
 from models import Bot
 from forms import UploadFileForm
 import os
+import subprocess
 
 def write_file_to_disk(uploaded_file, path):
     with open(os.path.join(path,uploaded_file.name), 'wb+') as dest:
         for chunk in uploaded_file.chunks():
             dest.write(chunk)
+
+    if(uploaded_file.name.endswith("gz")):
+        subprocess.check_output("tar -xvf {}".format(uploaded_file.name), shell=True, cwd=path)
 
 @login_required
 def index(request):
